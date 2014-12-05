3117710fcc1e5cb4ed8f95d91e0ecc4f045944e0
Sebastian <sebablasko@gmail.com>
2014-12-05 18:40:57 -0300

    implementadas las funciones my_* para bind y connect en tcp y udp

--- ssocket.c
+++ ssocket.c
@@ -11,6 +11,9 @@
 #include "ssocket.h"
 
 
+
+#include <unistd.h>
+
 /* accept */
 int socket_accept(int s){
     struct sockaddr_storage from;
@@ -20,22 +23,42 @@ int socket_accept(int s){
 
 
 /* bind */
-int socket_socket_udp_bind(char *port) {
-	return socket_socket_bind(SOCK_DGRAM, port);
+int socket_udp_bind(char *port) {
+	return socket_bind(SOCK_DGRAM, port);
 }
 
-int socket_socket_tcp_bind(char *port) {
-	return socket_socket_bind(SOCK_STREAM, port);
+int socket_tcp_bind(char *port) {
+	return socket_bind(SOCK_STREAM, port);
 }
+
+/*
+int socket_unix_bind(char *port){
+    int s;
+    struct sockaddr_un addr;
+
+    s = socket(AF_UNIX, SOCK_DGRAM, 0);
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    strncpy(addr.sun_path, "socket", sizeof(addr.sun_path)-1);
+
+    if(bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "bind failed\n");
+        exit(1);
+   }
+
+   return s;
+}
+*/
 
 int socket_bind(int type, char *port){
     struct addrinfo hints, *res, *ressave;
     int n, sockfd;
     memset(&hints, 0, sizeof(struct addrinfo));
 
-    hints.ai_flags    = AI_PASSIVE | AI_V4MAPPED | AI_ALL;
     hints.ai_family   = AF_INET;
     hints.ai_socktype = type;
+    hints.ai_flags    = AI_PASSIVE | AI_V4MAPPED | AI_ALL;
 
     n = getaddrinfo(NULL, port, &hints, &res);
 
@@ -117,5 +140,118 @@ int socket_connect(int type, char *server, char *port){
     }
 
     freeaddrinfo(ressave);
+    return sockfd;
+}
+
+
+//---------------------------------
+
+int my_udp_bind(char *port){
+    int sockfd;
+    struct sockaddr_in addr;
+
+    //crear Socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        fprintf(stderr, "UDP socket error:: No se pudo crear el socket\n");
+        return -1;
+    }
+
+    //Set Family
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); //verificar que hace
+
+    if(bind(sockfd, (struct sockaddr*) &addr, sizeof(addr)) < 0){
+        fprintf(stderr, "socket error:: No se pudo hacer bind\n");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int my_udp_connect(char *server, char *port){
+    int sockfd;
+    struct sockaddr_in addr;
+
+    //crear Socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        fprintf(stderr, "socket error:: No se pudo crear el socket\n");
+        return -1;
+    }
+
+    //Set Family
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr.s_addr = inet_addr(server); //verificar que hace
+
+    if(connect(sockfd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "socket error:: No se pudo hacer connect\n");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int my_tcp_bind(char *port){
+    int sockfd, sockfd_accepted;
+    struct sockaddr_in addr, from;
+
+    //crear Socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
+        fprintf(stderr, "TCP socket error:: No se pudo crear el socket\n");
+        return -1;
+    }
+
+    //Set Family
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); //verificar que hace
+
+    if(bind(sockfd, (struct sockaddr*) &addr, sizeof(addr)) < 0){
+        fprintf(stderr, "socket error:: No se pudo hacer bind\n");
+        close(sockfd);
+        return -1;
+    }
+
+    listen(sockfd, 5);
+
+    int from_len = sizeof(from);
+    sockfd_accepted = accept(sockfd, (struct sockaddr*) &from, &from_len);
+    if (from_len<0){
+        fprintf(stderr, "socket error:: Error de accept\n");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd_accepted;
+}
+
+int my_tcp_connect(char *server, char *port){
+    int sockfd;
+    struct sockaddr_in addr;
+
+    //crear Socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
+        fprintf(stderr, "TCP socket error:: No se pudo crear el socket\n");
+        return -1;
+    }
+
+    //Set Family
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr.s_addr = inet_addr(server); //verificar que hace
+
+    if(connect(sockfd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "TCP socket error:: No se pudo hacer connect\n");
+        close(sockfd);
+        return -1;
+    }
+
     return sockfd;
 }
--- ssocket.h
+++ ssocket.h
@@ -1,12 +1,18 @@
 /* Compilado de funciones de manejo de sockets
- inspirada en la librería jsockets de piquer */
+ inspirada en la librería jsockets de piquer
+ y de la documentación en
+ http://www.ivanescobar.com/oper2_material/sockunix.pdf */
 
 int socket_accept(int s);
 
 int socket_udp_bind(char *port);
 int socket_tcp_bind(char *port);
-int socket_bind(int type, char *port);
+//int socket_unix_bind(char *port);
 
 int socket_udp_connect(char *server, char *port);
 int socket_tcp_connect(char *server, char *port);
-int socket_connect(int type, char *server, char *port);
+
+int my_udp_bind(char *port);
+int my_udp_connect(char *server, char *port);
+int my_tcp_bind(char *port);
+int my_tcp_connect(char *server, char *port);
