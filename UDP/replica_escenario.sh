58424fc5e0261b5d66c8e45f675aa0c586192cee
Sebastian <sebablasko@gmail.com>
2014-12-18 15:06:19 -0300

    script para verificar relacion clientes/servidores

---
+++ UDP/replica_escenario.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+for serverThreads in 1 2 4 8 16 32
+do
+	linea=$serverThreads";"
+	for clients in 1 2 3 4 5 6 7 8 9 10
+	do
+		echo $serverThreads" threads en servidor"
+		echo $clients" clientes corriendo"
+
+		repetitions=6
+		for ((i=1 ; $i<=$repetitions ; i++))
+		{
+			./server $serverThreads 1 >> aux &
+			pid=$!
+			sleep 1
+
+			for ((j=1 ; $j<=$clients ; j++))
+			{
+				./client 1 127.0.0.1 > /dev/null &
+			}
+			wait $pid
+			linea="$linea$(cat aux)"
+			rm aux
+		}
+		linea="$linea;"
+	done
+	echo "$linea" >> cosa.csv
+done
