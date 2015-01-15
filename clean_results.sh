9d11d53bbf22d84489186e134466f409b7945361
Sebastian <sebablasko@gmail.com>
2015-01-15 19:44:03 -0300

    aumento el clean para borrar carpetas de callgraphs

--- clean_results.sh
+++ clean_results.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 rm -r DEV_NULL/perf*
+rm -r DEV_NULL/callgraphs*
 rm -r DEV_URANDOM/perf*
+rm -r DEV_URANDOM/callgraphs*
 rm -r FIFO/perf*
+rm -r FIFO/callgraphs*
 rm -r TCP/perf*
+rm -r TCP/callgraphs*
 rm -r UNIX/perf*
+rm -r UNIX/callgraphs*
 rm -r UDP/perf*
+rm -r UDP/callgraphs*
 rm -r RESULTS
 rm *.o
